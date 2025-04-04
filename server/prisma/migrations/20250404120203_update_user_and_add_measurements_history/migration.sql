adad4c949de315bfdeccb3cf88ee1dda9ef465f8
Halvor Etterdal <halvoret@stud.ntnu.no>
2025-04-04 14:02:26 +0200

    update_user_and_add_measurements_history

---
+++ server/prisma/migrations/20250404120203_update_user_and_add_measurements_history/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `address` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `birthday` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `occupation` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `phone` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `website` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "address",
+DROP COLUMN "birthday",
+DROP COLUMN "occupation",
+DROP COLUMN "phone",
+DROP COLUMN "website";
+
+-- CreateTable
+CREATE TABLE "BodyMeasurement" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "weight" DOUBLE PRECISION,
+    "height" DOUBLE PRECISION,
+    "bodyFat" DOUBLE PRECISION,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "BodyMeasurement_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "BodyMeasurement_userId_date_idx" ON "BodyMeasurement"("userId", "date");
+
+-- AddForeignKey
+ALTER TABLE "BodyMeasurement" ADD CONSTRAINT "BodyMeasurement_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
