64117abb7b76831edcc348111b324a2a22464e02
Halvor Etterdal <halvoret@stud.ntnu.no>
2025-04-18 12:53:17 +0200

    updated workouts almost

---
+++ server/prisma/migrations/20250417224712_added_completed_for_sets/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "DropsetSubSet" ADD COLUMN     "completed" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "ExerciseSet" ADD COLUMN     "completed" BOOLEAN NOT NULL DEFAULT false;
