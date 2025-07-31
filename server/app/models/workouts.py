f09df51f3305487d0570f6b49c455afae7e690d9
Halvor Etterdal <halvoret@stud.ntnu.no>
2025-07-31 21:52:36 +0200

    added mode josn to model dumps in api

--- server/app/models/workouts.py
+++ server/app/models/workouts.py
@@ -1,41 +1,53 @@
 from pydantic import BaseModel
 from typing import List
 
+# ------------------------------------
+# ----------- Subset -----------------
+# ------------------------------------
+
 class SubsetData(BaseModel):
     reps: int
     subset_number: int
     weight: float
+
+class SubsetCreate(SubsetData):
+    set_id: int
+
+
+# ------------------------------------
+# ------------- Set ------------------
+# ------------------------------------
+
 
 class SetData(BaseModel):
     exercise_name: str
     set_number: int
     sub_sets: List[SubsetData] = []
 
+class SetCreate(SetData):
+    exercise_id: int
+
+# ------------------------------------
+# ----------- Exercise ---------------
+# ------------------------------------
+
+
+
 class ExerciseData(BaseModel):
     exercise_number: int
     sets: List[SetData] = []
 
-class WorkoutData(BaseModel):
-    user_id: int
-    exercises: List[ExerciseData] = []
-
-class SubsetCreate(SubsetData):
-    set_id: int
-
-    class Config:
-        from_attributes=True
-
-class SetCreate(SetData):
-    exercise_id: int
-
-    class Config:
-        from_attributes=True
-
 class ExerciseCreate(ExerciseData):
     workout_id: int
 
-    class Config:
-        from_attributes=True
+# ------------------------------------
+# ----------- Workout ----------------
+# ------------------------------------
+
+
+class WorkoutData(BaseModel):
+    user_id: int
+    exercises: List[ExerciseData] = []
 
 class WorkoutCreate(WorkoutData):
     class Config:
