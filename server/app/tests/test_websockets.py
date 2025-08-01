582fe67d76b9094ed772eb96a0d43c48cab5ddb6
Halvor Etterdal <halvoret@stud.ntnu.no>
2025-08-01 21:53:00 +0200

    added websocket test - exercise created, user broadcast

--- server/app/tests/test_websockets.py
+++ server/app/tests/test_websockets.py
@@ -1,5 +1,9 @@
 from tests.util import create_workout, subscribe_and_listen, retrieve_workout
+from fastapi.testclient import TestClient
+from main import app
 import pytest
+
+client = TestClient(app)
 
 @pytest.fixture(scope="module")
 def data():
@@ -12,3 +16,11 @@ def test_workout_created_user_broadcast(data):
 
     assert message["type"] == "workout_created"
     assert message["data"]["user_id"] == created["user_id"]
+
+def test_exercise_created_user_broadcast(data):
+    with subscribe_and_listen("user:1") as ws:
+        created = client.post(f"/api/exercises", json={**data["workout"]["exercises"][0], "workout_id":1}).json()
+        message = ws.receive_json()
+
+    assert message["type"] == "exercise_created"
+    assert message["data"]["workout_id"] == created["workout_id"]
